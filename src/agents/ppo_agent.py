1594f55203ff6a75f81f8d6fff36514624780f82
Rahul <rahul.bhatia.k@gmail.com>
2020-12-16 21:34:10 -0800

    updating ppo_agent to have some more details filled in

--- src/agents/ppo_agent.py
+++ src/agents/ppo_agent.py
@@ -6,11 +6,42 @@ Implementation of an RLcard agent using the Proximal Policy Optimization method
 class PPOAgent(object):
     """A PPO RL Agent in the RLCard environment"""
     
-    def __init__(self):
-        raise NotImplementedError
+    def __init__(self, train_every):
+        self.train_every = train_every
+        self.memory = []
+        self.total_t = 0
 
     @property
     def use_raw(self):
+        raise NotImplementedError
+    
+    def feed(self, ts):
+        ''' Store data in to replay buffer and train the agent. There are two stages.
+            In stage 1, populate the memory without training
+            In stage 2, train the agent every several timesteps
+
+        Args:
+            ts (list): a list of 5 elements that represent the transition
+        '''
+        (state, action, reward, next_state, done) = tuple(ts)
+        self.feed_memory(state['obs'], action, reward, next_state['obs'], done)
+        self.total_t += 1
+        tmp = self.total_t - self.replay_memory_init_size
+        if tmp>=0 and tmp%self.train_every == 0:
+            self.train()
+
+    def feed_memory(self, state, action, reward, next_state, done):
+        '''Store the experience'''
+        self.memory.append({
+            "state": state,
+            "action": action,
+            "reward": reward,
+            "next_state": next_state,
+            "done": done,
+        })
+
+    def train(self):
+        ''' Train the model'''
         raise NotImplementedError
 
     def step(self, state: dict):
